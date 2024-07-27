27729e77c4a887a3079ecc8b3656c96fcdef6ceb
PhantomEnigma <1212ravi@proton.me>
2024-07-27 22:31:31 +0530

    Remove few apps from removepackages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -12,27 +12,7 @@ LOCAL_OVERRIDES_PACKAGES := \
     YouTube \
     BetterBugStub \
     CalculatorGooglePrebuilt \
-    CalendarGooglePrebuilt \
-    Chrome-Stub \
-    GoogleContacts \
-    PrebuiltDeskClockGoogle \
-    PrebuiltBugle \
-    TrichromeLibrary \
-    TrichromeLibrary-Stub \
-    GCS \
-    talkback \
-    WellbeingPrebuilt \
-    GoogleDialer \
-    DeviceIntelligenceNetworkPrebuilt \
-    DevicePersonalizationPrebuiltPixel2020 \
-    SafetyHubPrebuilt \
-    GoogleTTS \
-    TurboPrebuilt \
-    LatinIMEGooglePrebuilt \
-    WebViewGoogle \
-    WebViewGoogle-Stub \
-    HealthIntelligenceStubPrebuilt \
-    QuickSearchBox
+    CalendarGooglePrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
