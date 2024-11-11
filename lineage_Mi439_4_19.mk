ac566c22d2f127efe743c32704b32803107b0964
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-11 23:04:44 +0600

    #Vanillabuild

--- lineage_Mi439_4_19.mk
+++ lineage_Mi439_4_19.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common RisingOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Kernel
@@ -34,7 +34,14 @@ PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 ROM_FOLDER := lineage
-#WITH_GMS := false
+
+# RisingOS stuff 
+TARGET_BOOT_ANIMATION_RES := 729
+TARGET_ENABLE_BLUR := true
+#RISING_OFFICIAL := true
+#WITH_GMS := true
+RISING_MAINTAINER := AFK FARHAN
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Signing
 -include vendor/lineage-priv/keys/keys.mk
@@ -44,3 +51,8 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="olive-user 10 QKQ1.191014.001 V12.5.1.0.QCNMIXM release-keys"
     BuildFingerprint=Xiaomi/olive/olive:10/QKQ1.191014.001/V12.5.1.0.QCNMIXM:user/release-keys
+
+# Rising specific prop overrides
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RisingChipset="SDM 439" \
+    RisingMaintainer="AFK FARHAN"
