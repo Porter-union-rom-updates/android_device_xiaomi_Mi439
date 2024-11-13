8ba1b14b4cbd0912963ea0a68f92d6dbabf34c2c
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-13 10:14:44 +0600

    @Being-up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,17 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2024-2025 The VoltageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+      $(LOCAL_DIR)/voltage_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    voltage_Mi439_4_19-user \
+    voltage_Mi439_4_19-userdebug \
+    voltage_Mi439_4_19-eng
