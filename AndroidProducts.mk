4703ae337605be9f5f99e1b04eb8757091fb8355
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-28 22:32:32 +0600

    Added

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,12 +6,12 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_Mi439.mk \
-    $(LOCAL_DIR)/lineage_Mi439_4_19.mk
+    $(LOCAL_DIR)/superior_Mi439_4_19.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Mi439-user \
-    lineage_Mi439-userdebug \
-    lineage_Mi439-eng \
-    lineage_Mi439_4_19-user \
-    lineage_Mi439_4_19-userdebug \
-    lineage_Mi439_4_19-eng
+    superior_Mi439-user \
+    superior_Mi439-userdebug \
+    superior_Mi439-eng \
+    superior_Mi439_4_19-user \
+    superior_Mi439_4_19-userdebug \
+    superior_Mi439_4_19-eng
