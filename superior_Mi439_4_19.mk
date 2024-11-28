3a4dac180cebcd139c2b4364f89ef1e8d0f31783
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-11-28 22:31:16 +0600

    Wtf

--- superior_Mi439_4_19.mk
+++ superior_Mi439_4_19.mk
@@ -33,8 +33,8 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SDM439
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
-ROM_FOLDER := lineage
-WITH_GMS := superior
+ROM_FOLDER := superior
+WITH_GMS := false 
 
 # Signing
 -include vendor/lineage-priv/keys/keys.mk
