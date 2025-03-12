36619d42da9efa8596f8731218ba6dbc1d35b719
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-13 06:13:10 +0800

    Mi439: Stop building MIUI Camera for 15 QPR2
    panic: can't extend property "relative_install_path": failed to find property to extend
    Change-Id: I8ca0520d879d9ab1c152ade18aa49276d3a61db5

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # EXTRA: MiuiCamera
 ifneq ($(wildcard vendor/miuicamera/config.mk),)
-$(call inherit-product, vendor/miuicamera/config.mk)
+#$(call inherit-product, vendor/miuicamera/config.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/miuicam/pine.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/pine.xml \
     $(LOCAL_PATH)/configs/miuicam/olive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/device_features/olive.xml \
