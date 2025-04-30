b7e904fb356ffbd4adf9282397d72f6f84d6670e
Gtajisan <124022055+Gtajisan@users.noreply.github.com>
2025-04-30 03:17:58 +0000

    infinity extra
    Signed-off-by: Gtajisan <124022055+Gtajisan@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -163,3 +163,7 @@ BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/Mi439/BoardConfigVendor.mk
+# Missing Required Module
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/Phoneinfo.prop
